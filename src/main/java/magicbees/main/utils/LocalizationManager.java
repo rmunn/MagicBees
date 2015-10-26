e5a26e1b88c200b5e557e03e68b6abd6d8af1893
Hexicube <Hexicube@users.noreply.github.com>
2015-10-26 22:07:42 +0000

    Fixed bracing
    Top kek dev :D

--- src/main/java/magicbees/main/utils/LocalizationManager.java
+++ src/main/java/magicbees/main/utils/LocalizationManager.java
@@ -6,13 +6,25 @@ public class LocalizationManager
 {
 	public static String getLocalizedString(String key)
 	{
-		if(StatCollector.canTranslate(key)) return StatCollector.translateToLocal(key);
-		else return StatCollector.translateToFallback(key);
+		if(StatCollector.canTranslate(key))
+		{
+			return StatCollector.translateToLocal(key);
+		}
+		else
+		{
+			return StatCollector.translateToFallback(key);
+		}
 	}
 
 	public static String getLocalizedString(String key, Object... objects)
 	{
-		if(StatCollector.canTranslate(key)) return String.format(StatCollector.translateToLocal(key), objects);
-		else return String.format(StatCollector.translateToFallback(key), objects);
+		if(StatCollector.canTranslate(key))
+		{
+			return String.format(StatCollector.translateToLocal(key), objects);
+		}
+		else
+		{
+			return String.format(StatCollector.translateToFallback(key), objects);
+		}
 	}
 }
